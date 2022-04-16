f8e4b11838ea8ad2db75bede3e73431bf71d40ab
Reginaldo de Araujo Oliveira <rao.corretor@gmail.com>
2022-04-16 17:56:42 -0300

    15º Dia
    Refazendo index.html e index.css

--- js/total.js
+++ js/total.js
@@ -1,7 +1,7 @@
 var $input_quantidade = document.querySelector("#qtd");
-var $output_total =document.querySelector ("#total");
+var $output_total = document.querySelector ("#total");
 
-$input_quantidade.oninput= function(){
+$input_quantidade.oninput = function(){
     var preco = document.querySelector ("#preco").textContent;
     preco = preco.replace ("R$", "");
     preco = preco.replace (",",".");
